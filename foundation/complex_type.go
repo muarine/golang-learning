4a0eaae43296cf92fa0cb3ed5f5e8b517415a190
Muarine <maoyun@rtmap.com>
2017-04-05 13:59:17 +0800

    复杂类型 slice数组操作特性

--- foundation/complex_type.go
+++ foundation/complex_type.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
@@ -37,4 +40,56 @@ func main() {
 	ss.sex = "女"
 	fmt.Println(ss)
 
+	// 二维数组，
+	game := [][]string{
+		[] string{"_","_","_"},
+		[] string{"_","_","_"},
+		[] string{"_","_","_"},
+	}
+
+	game[0][0] = "X"
+	game[1][2] = "X"
+	game[2][2] = "X"
+
+	for i:= 0; i<len(game); i++ {
+		fmt.Printf("%s\n", strings.Join(game[i]," "))
+	}
+
+	// slice  没有固定长度、内部由array指针、len长度、cap容量构成
+	// append函数在现有长度下增长是翻倍的<1024, >1024后以1.25倍数增长
+	// 选择slice元素,[start,end-1]
+
+	s := []int{1,2,3,4,5,6}
+	s1 := s[0:2]
+	fmt.Println(s1)  // [1 2]
+	s2 := s[4:]
+	fmt.Println(s2)  // [5 6]
+	s3 := s[:4]
+	fmt.Println(s3)  // [1 2 3 4]
+
+	s = append(s , 7)
+
+	fmt.Println(s)
+
+
+	// slice 考点
+	slice1 := []int{1,2,3,4,5}
+	fmt.Println("初始化的slice数组", slice1)
+	funSlice1(slice1)
+	fmt.Println("funSlice1 操作后", slice1)
+	funSlice2(&slice1)
+	fmt.Println("funSlice2 操作后", slice1)
+
+}
+
+func funSlice1(slice []int) {
+	slice[1] = 2222
+	slice = append(slice, 6666)
+	fmt.Printf("funSlice1-->data:\t%#v\n", slice)
+	fmt.Printf("funSlice1-->len:\t%#v\n", len(slice))
+	fmt.Printf("funSlice1-->cap:\t%#v\n", cap(slice))
+}
+
+func funSlice2(slice2 *[]int){
+	*slice2 = append(*slice2, 66666)
 }
