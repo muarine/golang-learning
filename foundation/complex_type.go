94af6a8daa8729c741a67701038b4fca72fa6357
Muarine <maoyun@rtmap.com>
2017-04-05 23:06:00 +0800

    1. 把函数作为值传递 2. 闭包函数 3. 体验map集合

--- foundation/complex_type.go
+++ foundation/complex_type.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"strings"
+	"math"
+	"sort"
 )
 
 func main() {
@@ -61,7 +63,7 @@ func main() {
 
 	s := []int{1,2,3,4,5,6}
 	s1 := s[0:2]
-	fmt.Println(s1)  // [1 2]
+	fmt.Println(s1)  // [1 2]	从下标0开始计数,直到end位，不含end位
 	s2 := s[4:]
 	fmt.Println(s2)  // [5 6]
 	s3 := s[:4]
@@ -76,10 +78,113 @@ func main() {
 	slice1 := []int{1,2,3,4,5}
 	fmt.Println("初始化的slice数组", slice1)
 	funSlice1(slice1)
-	fmt.Println("funSlice1 操作后", slice1)
+	fmt.Println("funSlice1 操作后", slice1)	// 数组内为共用引用所以slice1中的2变成了2222
 	funSlice2(&slice1)
-	fmt.Println("funSlice2 操作后", slice1)
+	fmt.Println("funSlice2 操作后", slice1)	// &指向了slice1的指针，跨函数操作也操作slice1本身
 
+	// slice 由函数 make 创建。这会分配一个全是零值的数组并且返回一个 slice 指向这个数组
+	a := make([]int, 5)
+	printSlice("a", a)	// [0 0 0 0 0]
+	b := make([]int, 0, 5)
+	printSlice("b", b)	// 指定size和cap容量
+	c := b[:2]
+	printSlice("c", c)	// [0 0]
+	d := c[2:5]
+	printSlice("d", d)	// [0 0 0]
+
+
+	var z []int
+	fmt.Println(z, len(z), cap(z))
+	if z == nil {
+		fmt.Println("nil!")
+	}
+
+	lll := make([]int, 5)
+	lll = append(lll, 1,2,3,4,5,6,7)
+	fmt.Printf("%s len:%d cap:%d %v\n", "lll",len(lll), cap(lll), lll)
+
+
+	// 函数值 函数也可作为参数传递
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+	fmt.Println(hypot(5,12))
+	fmt.Println(complexFunc(hypot))
+	fmt.Println(complexFunc(math.Pow))
+
+	// 闭包，作为一个函数值，引用了函数之外的变量，这个函数可以对引用的变量进行访问和赋值,换句话说，这个函数被“绑定”在这个变量上。
+	var add,neg = adder(),adder()
+	for i:=0;i<10 ;i++  {
+		fmt.Println(i,add(i),neg(-2*i))
+	}
+
+
+	// 闭包实现fibonacci数列
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+
+	// map集合
+	map1 := make(map[string]string)
+
+	// 创建
+	map1["title"] = "张三"
+	map1["count"] = "1"
+	// 读取
+	fmt.Println(map1["title"])
+	// 更新
+	map1["title"] = "李斯"
+	fmt.Println(map1["title"])
+	// 删除
+	delete(map1, "title")
+
+	// 遍历
+	for key,value := range map1{
+		fmt.Printf("%s %s \n", key, value)
+	}
+
+	// 验证map是否有序
+	progameLanguages := map[string]int{
+		"unix": 	0,
+		"python": 	1,
+		"go": 		2,
+		"javascript": 	3,
+		"testing": 	4,
+		"philosophy": 	5,
+		"startups": 	6,
+		"productivity": 7,
+		"hn": 		8,
+		"reddit": 	9,
+		"C++": 		10,
+	}
+
+	// 从go 1版本开始，map的遍历顺序是随机的
+	for key,value := range progameLanguages  {
+		fmt.Printf("%s %d\n", key, value)
+	}
+
+	var keys []string
+
+	for k := range progameLanguages {
+		keys = append(keys, k)
+	}
+	// 可以利用sort根据ASCII排序，再根据键读取值的特性
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		fmt.Println("Key:", k, "Value:", progameLanguages[k])
+	}
+
+}
+
+func adder() func(int) int{
+	var sum = 0
+	return func(x int) int {
+		sum += x
+		fmt.Printf("sum:%d x:%d\n", sum, x)
+		return sum
+	}
 }
 
 func funSlice1(slice []int) {
@@ -92,4 +197,39 @@ func funSlice1(slice []int) {
 
 func funSlice2(slice2 *[]int){
 	*slice2 = append(*slice2, 66666)
+}
+
+func printSlice(s string, slice []int)  {
+	fmt.Printf("%s len:%d cap:%d slice:%v\n", s, len(slice), cap(slice), slice)
+}
+
+func complexFunc(f func(float1,float2 float64) float64) float64 {
+	return f(3,4)
+}
+
+var slice = make([]int,11)
+
+func fibonacci() func() int {
+	var i = 0
+	return func() int{
+		switch i {
+		case 0:
+			slice[0]=0
+			i++
+			return 0
+		case 1:
+			slice[1]=1
+			i++
+			return 1
+		case 2:
+			slice[2] = slice[i-1] + slice[i-2]
+			i++
+			return slice[2]
+		default:
+			var j = i
+			slice[j] = slice[i-1] + slice[i-2]
+			i++
+			return slice[j]
+		}
+	}
 }
